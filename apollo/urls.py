037402c8ad415b61b09e60a22cf28e8f6112a08b
lcc184 <2922898462@qq.com>
2020-07-22 13:42:15 +0800

    实现了A等级的安全级别

--- apollo/urls.py
+++ apollo/urls.py
@@ -2,5 +2,6 @@ from apollo import views
 from django.urls import path
 
 urlpatterns = [
-    path('index/', views.index),
+    path('index/', views.index), # 配置访问首页的路径
+    path('safe_a/', views.safe_a), # 配置访问a等级安全的路由配置路径
 ]
